b91cb1da7e5579fce6475a3707ba8281b475e454
asheeq <asheeq.bracu@gmail.com>
2020-05-12 11:51:38 +0600

    error fixed and updated

--- validator/product-category-rollup.js
+++ validator/product-category-rollup.js
@@ -4,18 +4,18 @@ module.exports = (ValidationSchema, Joi) => {
   return [
     ValidationSchema.define('CategoryRollupFetch', Joi.object().keys({
       partOf: Joi.number().integer().max(20).optional(),
-      madeOf: Joi.number().integer().max(20).optional(),
+      madeUpOf: Joi.number().integer().max(20).optional(),
       status: Joi.number().integer().valid(0, 1).optional()
     })),
     ValidationSchema.define('CategoryRollupUpdate', Joi.object().keys({
       partOf: Joi.number().integer().max(20).optional(),
-      madeOf: Joi.number().integer().max(20).optional(),
+      madeUpOf: Joi.number().integer().max(20).optional(),
       status: Joi.number().integer().valid(0, 1).optional(),
       updatedBy: Joi.string().max(255).required()
     })),
     ValidationSchema.define('CategoryRollupCreate', Joi.object().keys({
       partOf: Joi.number().integer().max(20).required(),
-      madeOf: Joi.number().integer().max(20).required(),
+      madeUpOf: Joi.number().integer().max(20).required(),
       status: Joi.number().integer().valid(0, 1).required(),
       createdBy: Joi.string().max(255).required()
     }))
