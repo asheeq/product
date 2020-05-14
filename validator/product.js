a18413b205a857bfe4bf1d232c1bdc5f24253b3a
asheeq <asheeq.bracu@gmail.com>
2020-05-14 19:26:30 +0600

    changes made in product validator and lib

--- validator/product.js
+++ validator/product.js
@@ -4,20 +4,25 @@ module.exports = (ValidationSchema, Joi) => {
   return [
     ValidationSchema.define('ProductFetch', Joi.object().keys({
       id: Joi.number().integer().optional(),
-      name: Joi.string().max(255).optional()
+      name: Joi.string().max(255).optional(),
+      startDate: Joi.date().format('YYYY-MM-DD').options({convert: true}).optional(),
+      endDate: Joi.date().format('YYYY-MM-DD').min(Joi.ref('startDate')).options({convert: true}).optional(),
+      offset: Joi.number().integer().min(0).options({convert: true}).optional(),
+      limit: Joi.number().integer().min(1).options({convert: true}).optional()
     })),
     ValidationSchema.define('ProductUpdate', Joi.object().keys({
       id: Joi.number().integer().min(1).required(),
-      name: Joi.string().max(255).required(),
+      name: Joi.string().max(255).optional(),
       description: Joi.string().max(1024).optional(),
       status: Joi.number().integer().valid(0, 1).optional(),
-      updatedBy: Joi.string().max(255).required(),
-      salesDiscontinueAt: Joi.date().format('YYYY-MM-DD').options({convert: true}).optional()
-    }).or('status','description','salesDiscontinueAt')),
+      salesDiscontinueAt: Joi.date().format('YYYY-MM-DD HH:mm:ss').options({convert: true}).optional(),
+      updatedBy: Joi.string().max(255).required()
+    }).or('name','status','description','salesDiscontinueAt')),
     ValidationSchema.define('ProductCreate', Joi.object().keys({
       name: Joi.string().max(255).required(),
       description: Joi.string().max(1024).optional(),
-      introduceAt: Joi.date().format('YYYY-MM-DD').options({convert: true}).required(),
+      introduceAt: Joi.date().format('YYYY-MM-DD HH:mm:ss').options({convert: true}).optional(),
+      salesDiscontinueAt: Joi.date().format('YYYY-MM-DD HH:mm:ss').options({convert: true}).optional(),
       createdBy: Joi.string().max(255).required()
     }))
   ];
