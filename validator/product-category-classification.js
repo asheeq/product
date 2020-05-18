f8fd27dfaba3bd4e77a93a28b2a2886d286cdb79
asheeq <asheeq.bracu@gmail.com>
2020-05-18 14:14:10 +0600

    product-category-classification updated

--- validator/product-category-classification.js
+++ validator/product-category-classification.js
@@ -6,25 +6,25 @@ module.exports = (ValidationSchema, Joi) => {
       id: Joi.number().integer().optional(),
       productId: Joi.number().integer().optional(),
       categoryId: Joi.number().integer().optional(),
-      isPrimary: Joi.string().valid('true','false').optional(),
-      fromDate: Joi.date().format('YYYY-MM-DD').options({convert: true}).optional(),
-      thruDate: Joi.date().format('YYYY-MM-DD').min(Joi.ref('fromDate')).options({convert: true}).optional()
+      isPrimary: Joi.boolean().optional(),
+      fromDate: Joi.date().format('YYYY-MM-DD HH:mm:ss').options({convert: true}).optional(),
+      thruDate: Joi.date().format('YYYY-MM-DD HH:mm:ss').min(Joi.ref('fromDate')).options({convert: true}).optional()
     })),
     ValidationSchema.define('ProductClassificationUpdate', Joi.object().keys({
       id: Joi.number().integer().min(1).required(),
-      productId: Joi.number().integer().optional(),
-      categoryId: Joi.number().integer().optional(),
-      isPrimary: Joi.string().valid('true','false').optional(),
-      fromDate: Joi.date().format('YYYY-MM-DD').options({convert: true}).optional(),
-      thruDate: Joi.date().format('YYYY-MM-DD').min(Joi.ref('fromDate')).options({convert: true}).optional(),
+      productId: Joi.number().integer().min(1).optional(),
+      categoryId: Joi.number().integer().min(1).optional(),
+      isPrimary: Joi.boolean().optional(),
+      fromDate: Joi.date().format('YYYY-MM-DD HH:mm:ss').options({convert: true}).optional(),
+      thruDate: Joi.date().format('YYYY-MM-DD HH:mm:ss').min(Joi.ref('fromDate')).options({convert: true}).optional(),
       updatedBy: Joi.string().max(255).required()
     }).or('productId','categoryId','isPrimary','fromDate','thruDate')),
     ValidationSchema.define('ProductClassificationCreate', Joi.object().keys({
-      productId: Joi.number().integer().required(),
-      categoryId: Joi.number().integer().required(),
-      isPrimary: Joi.string().valid('true','false').required(),
-      fromDate: Joi.date().format('YYYY-MM-DD').options({convert: true}).optional(),
-      thruDate: Joi.date().format('YYYY-MM-DD').min(Joi.ref('fromDate')).options({convert: true}).optional(),
+      productId: Joi.number().integer().min(1).required(),
+      categoryId: Joi.number().integer().min(1).required(),
+      isPrimary: Joi.boolean().optional(),
+      fromDate: Joi.date().format('YYYY-MM-DD HH:mm:ss').options({convert: true}).optional(),
+      thruDate: Joi.date().format('YYYY-MM-DD HH:mm:ss').min(Joi.ref('fromDate')).options({convert: true}).optional(),
       createdBy: Joi.string().max(255).required()
     }))
   ];
