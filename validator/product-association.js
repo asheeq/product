956d5d4c2db97b2e1ee4865dec0d961a1d747a93
asheeq <asheeq.bracu@gmail.com>
2020-05-20 13:28:18 +0600

    product association updated

--- validator/product-association.js
+++ validator/product-association.js
@@ -6,7 +6,7 @@ module.exports = (ValidationSchema, Joi) => {
       id: Joi.number().integer().min(1).optional(),
       type: Joi.string().valid('Component', 'Complement', 'Incompatible').optional(),
       associatedWith: Joi.number().integer().min(1).optional(),
-      associated: Joi.number().integer().min(1).disallow(Joi.ref('associatedWith')).optional(),
+      associated: Joi.number().integer().min(1).optional(),
       startDate: Joi.date().format('YYYY-MM-DD').options({convert: true}).optional(),
       endDate: Joi.date().format('YYYY-MM-DD').min(Joi.ref('startDate')).options({convert: true}).optional(),
       offset: Joi.number().integer().min(0).optional(),
@@ -15,15 +15,13 @@ module.exports = (ValidationSchema, Joi) => {
     ValidationSchema.define('ProductAssociationUpdate', Joi.object().keys({
       id: Joi.number().integer().min(1).required(),
       type: Joi.string().valid('Component', 'Complement', 'Incompatible').optional(),
-      quantity: Joi.number().integer().min(0).optional(),
-      associatedWith: Joi.number().integer().min(1).optional(),
-      associated: Joi.number().integer().min(1).disallow(Joi.ref('associatedWith')).optional(),
+      quantity: Joi.number().integer().min(1).optional(),
       fromDate: Joi.date().format('YYYY-MM-DD HH:mm:ss').options({convert: true}).optional(),
       thruDate: Joi.date().format('YYYY-MM-DD HH:mm:ss').min(Joi.ref('fromDate')).options({convert: true}).optional(),
       updatedBy: Joi.string().max(255).required()
-    }).or('type','quantity','associatedWith','associated','fromDate','thruDate')),
+    }).or('type','quantity','fromDate','thruDate')),
     ValidationSchema.define('ProductAssociationCreate', Joi.object().keys({
-      quantity: Joi.number().integer().min(0).optional(),
+      quantity: Joi.number().integer().min(1).optional(),
       type: Joi.string().valid('Component', 'Complement', 'Incompatible').optional(),
       associatedWith: Joi.number().integer().min(1).required(),
       associated: Joi.number().integer().min(1).disallow(Joi.ref('associatedWith')).required(),
